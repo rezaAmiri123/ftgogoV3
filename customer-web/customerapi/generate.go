ecd7280a56f87b785900369e403a9adabd0514ad
Reza Amiri <reza.amiri.ac@gmail.com>
2024-10-23 11:14:36 +0300

    add rest

---
+++ customer-web/customerapi/generate.go
@@ -0,0 +1,7 @@
+package customerapi
+
+// To regenerate the API and a Chi server use the following generate command
+//go:generate oapi-codegen -config oapi-codegen.cfg.yaml openapi.yaml
+//
+// The generated Chi server components are not used to construct the server it
+// is instead used to verify our web handlers cover the API completely.
