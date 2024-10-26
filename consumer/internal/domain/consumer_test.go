cc887718dfd6fe576237d1cbfa15a0c65c4f9a2a
Reza Amiri <reza.amiri.ac@gmail.com>
2024-10-26 12:05:44 +0300

    add unit test to consumer domain

--- consumer/internal/domain/consumer_test.go
+++ consumer/internal/domain/consumer_test.go
@@ -7,31 +7,89 @@ import (
 )
 
 func TestConsumer_GetAddress(t *testing.T) {
+	address := Address{Street1: "street1"}
 	type fields struct {
-		ID        string
-		Name      string
 		Addresses map[string]Address
 	}
-	type args struct{
+	type args struct {
 		id string
 	}
-	type want struct{
-		Address Address
-		err error
+	type want struct {
+		address Address
+		err     error
 	}
-	tests := map[string]struct{
+	tests := map[string]struct {
 		fields fields
-		args args
-	}{}
+		args   args
+		want   want
+	}{
+		"OK": {
+			fields: fields{Addresses: map[string]Address{"id": address}},
+			args:   args{id: "id"},
+			want:   want{address: address, err: nil},
+		},
+		"Not Exists": {
+			fields: fields{Addresses: map[string]Address{}},
+			args:   args{id: "id"},
+			want:   want{address: Address{}, err: ErrAddressDoesNotExist},
+		},
+	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			c := &Consumer{
-				ID: tt.fields.ID,
-				Name: tt.fields.Name,
-				Addresses: tt.fields.Addresses,
-			}
+			c := &Consumer{Addresses: tt.fields.Addresses}
 			got, err := c.GetAddress(tt.args.id)
-			
+			if tt.want.err != nil {
+				assert.Error(t, err)
+				assert.ErrorContains(t, err, tt.want.err.Error())
+				assert.Empty(t, got)
+				return
+			}
+			assert.Equal(t, got.Street1, tt.want.address.Street1)
+		})
+	}
+}
+
+func TestRegisterConsumer(t *testing.T) {
+	consumer := &Consumer{
+		ID:   "id",
+		Name: "name",
+	}
+	type args struct {
+		id   string
+		name string
+	}
+	type want struct {
+		consumer *Consumer
+		err      error
+	}
+	tests := map[string]struct {
+		args args
+		want want
+	}{
+		"OK": {
+			args: args{id: "id", name: "name"},
+			want: want{consumer: consumer, err: nil},
+		},
+		"ID is blank": {
+			args: args{id: "", name: "name"},
+			want: want{consumer: nil, err: ErrConsumerIDCannotBeBlank},
+		},
+		"Name is blank": {
+			args: args{id: "id", name: ""},
+			want: want{consumer: nil, err: ErrConsumerNameCannotBeBlank},
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := RegisterConsumer(tt.args.id, tt.args.name)
+			if tt.want.err != nil {
+				assert.Error(t, err)
+				assert.ErrorContains(t, err, tt.want.err.Error())
+				assert.Nil(t, got)
+				return
+			}
+			assert.Equal(t, got.ID, tt.want.consumer.ID)
+			assert.Empty(t, got.Addresses)
 		})
 	}
 }
