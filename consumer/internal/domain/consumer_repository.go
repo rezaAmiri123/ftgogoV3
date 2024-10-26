c70790943a913924cc357d5c100ba94c3fcee48c
Reza Amiri <reza.amiri.ac@gmail.com>
2024-10-26 15:15:17 +0300

    add consumer repository mock to consumer

--- consumer/internal/domain/consumer_repository.go
+++ consumer/internal/domain/consumer_repository.go
@@ -3,6 +3,7 @@ package domain
 
 import "context"
 
+//go:generate mockery --name ConsumerRepository
 type ConsumerRepository interface{
 	Save(ctx context.Context, consumer *Consumer)error
 	Find(ctx context.Context, consumerID string)(*Consumer,error)
